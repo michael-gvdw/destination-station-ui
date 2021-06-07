9358c085b492bc073080f2082123d48787f7d465
Michael Groenewegen van der Weijden <michael@fragrant-chios.com>
2021-06-07 13:00:38 +0200

    added dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={`container py-5`}>
+      <section>
+        <h1>Form</h1>
+        <div></div>
+      </section>
+      <section>
+        <h1>Options</h1>
+        <div></div>
+      </section>
+    </main>
   );
 }
 
