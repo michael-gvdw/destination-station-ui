a4034a62a984715d8febf5c71a3e2b9d9ba5df0c
Dutch994 <goedhartnick@gmail.com>
2021-06-08 10:10:35 +0200

    Minor Style Change

--- src/componenst/form/InputForm.js
+++ src/componenst/form/InputForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './StyleInputForm.css'
 
 const initialState={
     linkNS: "",
@@ -43,65 +44,80 @@ export default class InputForm extends React.Component{
     render(){
         return(
             <form onSubmit={this.handleSubmit}>
-                <div id="nsLink">
-                    <h2>NS Link</h2>
-                    <label>NS Link</label>
-                    <input
-                        type="text"
-                        name="linkNS"
-                        placeholder="NS Link"
-                        value={this.state.link}
-                        onChange={this.handleChange}
-                    />
+                <div class="form-row">
+                    <div class="form-group col-md-100">
+                        <label>NS Link</label>
+                        <input
+                            class = "form-control"
+                            type="text"
+                            name="linkNS"
+                            placeholder="NS Link"
+                            value={this.state.link}
+                            onChange={this.handleChange}
+                        />
+                    </div>
                 </div>
 
                 <div id="divInput">
                     <div id="inputToFrom">
-                        <div id="depFromInput">
-                            <h2>Departure Information</h2>
-                            <label>From</label>
-                            <input
-                                type="text"
-                                name="fromDep"
-                                placeholder="Depart From"
-                                value={this.state.from}
-                                onChange={this.handleChange}
-                            />
+                        <h2>Departure Information</h2>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">From</label>
+                            <div class="col-sm-10">
+                                <input
+                                    class="form-control"
+                                    type="text"
+                                    name="fromDep"
+                                    placeholder="Depart From"
+                                    value={this.state.from}
+                                    onChange={this.handleChange}
+                                />
+                                <div style={{fontSize: 12, color: "red"}}>{this.state.errorMes}</div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">To</label>
+                            <div class="col-sm-10">
+                                <input
+                                    class="form-control"
+                                    type="text"
+                                    name="toDes"
+                                    placeholder="To Destination"
+                                    value={this.state.to}
+                                    onChange={this.handleChange}
+                                />
+                            </div>
                             <div style={{fontSize:12, color:"red"}}>{this.state.errorMes}</div>
                         </div>
-                        <div id="desToInput">
-                            <label>To</label>
-                            <input
-                                type="text"
-                                name="toDes"
-                                placeholder="To Destination"
-                                value={this.state.to}
-                                onChange={this.handleChange}
-                            />
-                            <div style={{fontSize:12, color:"red"}}>{this.state.errorMes}</div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Date</label>
+                            <div class="col-sm-10">
+                                <input
+                                    class="form-control"
+                                    type="date"
+                                    name="dateDep"
+                                    value={this.state.date}
+                                    onChange={this.handleChange}
+                                />
+                                <div style={{fontSize:12, color:"red"}}>{this.state.errorMes}</div>
+                            </div>
                         </div>
-                    </div>
-                    <div id="inputDateTime">
-                        <div id="inputDate">
-                            <label>Date</label>
-                            <input
-                                type="date"
-                                name="dateDep"
-                                value={this.state.date}
-                                onChange={this.handleChange}
-                            />
-                            <div style={{fontSize:12, color:"red"}}>{this.state.errorMes}</div>
-                        </div>
-                        <div id="inputTime">
-                            <label>Time</label>
-                            <input
-                                type="time"
-                                name="timeDep"
-                                value={this.state.time}
-                                onChange={this.handleChange}
-                                min="00:00" max="23:59"
-                            />
-                            <div style={{fontSize: 12, color:"red"}}>{this.state.errorMes}</div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Time</label>
+                            <div class="col-sm-10">
+                                <input
+                                    class="form-control"
+                                    type="time"
+                                    name="timeDep"
+                                    value={this.state.time}
+                                    onChange={this.handleChange}
+                                    min="00:00" max="23:59"
+                                />
+                                <div style={{fontSize: 12, color:"red"}}>{this.state.errorMes}</div>
+                            </div>
                         </div>
                     </div>
                 </div>
